ec7eeead2ec87d4f81e86147bc385b7117f1fca1
Hoff Silva <hoff.henry@gmail.com>
2019-11-16 21:35:15 -0300

    Initial Commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Hoff Henry Pereira da Silva on 16/11/19.
+//  Copyright © 2019 Hoff Henry Pereira da Silva. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
